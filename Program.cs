e8f7b8d6dd72e71bea1296ed08d9181786e324da
ADEM SAMUK <65091977+ademsmk@users.noreply.github.com>
2021-09-19 08:13:45 +0300

    ELSE İF ELSE

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 10)
+                Console.WriteLine("Günaydın!");
+            else if (time <= 18)
+                Console.WriteLine("İyi Günler");
+            else
+                Console.WriteLine("İyi Geceler");
+
+            string result = time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            result = time >= 6 && time < 10 ? "Günaydın!" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            Console.WriteLine(result);
+        }
+    }
+}
