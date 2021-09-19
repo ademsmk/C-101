46be98845aa1c8a83500ca10751825838048f3db
ADEM SAMUK <65091977+ademsmk@users.noreply.github.com>
2021-09-19 07:37:47 +0300

    Try Catch kullanımı

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş Olduğunuz Sayı : " + sayi);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata : " + e.Message.ToString());
+            }
+
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı.");
+            }
+
+
+
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("test");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz.");
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri Tipi Uygun Değil.");
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı");
+            }
+        }
+    }
+}
