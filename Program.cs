f403eb31cf6d6dbcec48d53517671d915a8f86a4
ADEM SAMUK <65091977+ademsmk@users.noreply.github.com>
2021-09-19 09:05:46 +0300

    for loop

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
